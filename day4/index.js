c32d0cc1a65862ee9348f8436d774064c99b5b90
Rafid Hoda <rafidhoda@gmail.com>
2022-12-12 07:33:10 +0100

    day4 solve

---
+++ day4/index.js
@@ -0,0 +1,32 @@
+/* Whispering function
+Write a function `whisper` that takes in a sentence
+and returns a new sentence in all lowercase letters with
+"shh..." at the beginning.
+
+The function should also remove an exclamation point
+at the end of the sentence, if there is one.
+
+Example
+input: "The KITTENS are SLEEPING!"
+output: "shh... the kittens are sleeping"
+
+Hint: endsWith and slice
+*/
+
+function whisper(sentence) {
+    // make everything lowercase
+    let newSentence = sentence.toLowerCase()
+
+    // add shh... at the beginning
+    newSentence = `shh... ${newSentence}`
+
+    // if ! at the end, remove it
+    if (newSentence.endsWith("!")) {
+        newSentence = newSentence.slice(0, -1)
+    }
+
+    return newSentence
+}
+
+console.log(whisper("PLEASE STOP SHOUTING."));
+console.log(whisper("MA'AM, this is a Wendy's!"));
